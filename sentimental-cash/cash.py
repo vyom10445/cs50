10da991650a512f8dad7f65999d62873ea7103c2
vyom10445 <vyom.chaturvedi10445knp@gmail.com>
2024-06-18 01:57:15 +0000

    Tue, Jun 18, 2024, 7:27 AM +05:30

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -4,7 +4,6 @@
 
 from cs50 import get_float
 
-# get input from user as positive
 while True:
     try:
         input = get_float("Change Owed: ")
@@ -13,28 +12,22 @@ while True:
     except:
         print("", end="")
 
-# declare number of coins variable
 coins = 0
 
-# make change act like an int so it's more precise
 change = input * 100
 
-# count 25c coins
 while change >= 25:
     change = change - 25
     coins = coins + 1
 
-# count 10c coins
 while change >= 10:
     change = change - 10
     coins = coins + 1
 
-# count 5c coins
 while change >= 5:
     change = change - 5
     coins = coins + 1
 
-# count 1c coins
 while change >= 1:
     change = change - 1
     coins = coins + 1
